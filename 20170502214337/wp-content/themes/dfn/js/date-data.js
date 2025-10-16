2914b5d031273d95a938998a287b3bf2753a831a
Mike Felder <mfelder@microsoft.com>
2025-10-15 19:45:22 -0500

    Brought in 20170502214337 timestamp.

---
+++ 20170502214337/wp-content/themes/dfn/js/date-data.js
@@ -0,0 +1,30 @@
+<HTML>
+<HEAD>
+<TITLE>404 Not Found</TITLE>
+<BASE href="/error_docs/"><!--[if lte IE 6]></BASE><![endif]-->
+</HEAD>
+<BODY>
+<H1>Not Found</H1>
+The requested document was not found on this server.
+</BODY>
+</HTML><!--
+     FILE ARCHIVED ON 05:25:55 Dec 06, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 16:32:30 Oct 15, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+-->
+<!--
+playback timings (ms):
+  captures_list: 0.613
+  exclusion.robots: 0.031
+  exclusion.robots.policy: 0.02
+  esindex: 0.01
+  cdx.remote: 120.099
+  LoadShardBlock: 185.18 (3)
+  PetaboxLoader3.datanode: 385.538 (5)
+  PetaboxLoader3.resolve: 3549.775 (2)
+  load_resource: 3997.91
+  loaddict: 280.544
+-->
